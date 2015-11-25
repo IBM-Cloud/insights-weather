310a3d7693ec24b30d07b464e0b769aa241fd377
Frederic Lavigne <frederic.lavigne@fr.ibm.com>
2015-11-25 16:51:22 +0100

    added deployment tracker resolve #3

--- app.js
+++ app.js
@@ -4,6 +4,9 @@ var
   app = express(),
   cfenv = require('cfenv')
 
+//---Deployment Tracker---------------------------------------------------------
+require("cf-deployment-tracker-client").track();
+
 // load local VCAP configuration
 var vcapLocal = null
 try {
