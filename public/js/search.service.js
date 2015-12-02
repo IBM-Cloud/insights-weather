4a3d16c1edce66eebc1e767302376ae93455d555
Frederic Lavigne <frederic.lavigne@fr.ibm.com>
2015-12-02 10:39:23 +0100

    parse query into latitude and longitude resolve #5

--- public/js/search.service.js
+++ public/js/search.service.js
@@ -1,11 +1,31 @@
-/*global angular*/
+/*global angular,console*/
 (function () {
   "use strict";
 
+  // simple parser for longitude and latitude
+  var geocodeParser = /^([\-+]?[\d]{1,2}\.?\d+),(\s*[\-+]?[\d]{1,3}\.?\d+)$/;
+  
   function searchService($http, $q) {
     var callWeatherAPI = function (api, query) {
+
+      // by default we handle this as a simple query
+      var queryString = "q=" + encodeURIComponent(query);
+
+      // but we try to extract latitude and longitude from the query
+      try {
+        var geolocation = geocodeParser.exec(query);
+        if (geolocation && geolocation.length === 3) {
+          var latitude = parseFloat(geolocation[1]);
+          var longitude = parseFloat(geolocation[2]);
+          console.log("Extracted latitude", latitude, "and longitude", longitude);
+          queryString = "latitude=" + encodeURIComponent(latitude) + "&longitude=" + encodeURIComponent(longitude);
+        }
+      } catch (err) {
+        console.error(err);
+      }
+
       var deferred = $q.defer();
-      $http.get(api + "?q=" + encodeURIComponent(query)).success(function (data) {
+      $http.get(api + "?" + queryString).success(function (data) {
         deferred.resolve(data);
       }).error(function () {
         deferred.reject();
